e8212f335a3c703ff7d5460e9782c2c47c3867dc
Vmarjorie <vanessa.pinard@hotmail.fr>
2026-07-28 12:09:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mon-cockpit-v1-1';
+const CACHE='mon-cockpit-v1-2-3-revenus';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
